8eb2ade86a99a543cc1654c7e01639c3652a93eb
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-17 20:21:07 -0500

    0.1.25

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.RandomGuess.MixProject do
   def project do
     [
       app: :islands_client_random_guess,
-      version: "0.1.24",
+      version: "0.1.25",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client Random Guess",
@@ -46,11 +46,11 @@ defmodule Islands.Client.RandomGuess.MixProject do
     [
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
-      # To please dialyzer only...
+      # To please dialyzer...
       # {:islands_client_state, "~> 0.1"},
       {:islands_client_state, "~> 0.1", runtime: false},
       {:islands_coord, "~> 0.1"},
-      # To please dialyzer only...
+      # To please dialyzer...
       # {:islands_tally, "~> 0.1"},
       {:islands_tally, "~> 0.1", runtime: false},
       {:persist_config, "~> 0.4", runtime: false}
