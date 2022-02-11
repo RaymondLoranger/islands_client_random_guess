e7d46cc66b767d0ec058014d2dea5af49864d2ad
RaymondLoranger <Raymond.Loranger@bell.net>
2022-02-11 12:34:22 -0500

    0.1.31

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.RandomGuess.MixProject do
   def project do
     [
       app: :islands_client_random_guess,
-      version: "0.1.30",
+      version: "0.1.31",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client Random Guess",
